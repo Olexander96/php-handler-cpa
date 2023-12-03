07a21a6ef8ee3a4f89cb7d2cbb79dee76be2ff96
oleksandr-sheiko <104017886+Olexander96@users.noreply.github.com>
2023-12-03 14:49:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, GET");
 
 $message = "Hello from server";
